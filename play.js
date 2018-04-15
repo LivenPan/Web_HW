441fac72454dceb9f6c48373e4b62ec42161bf2d
tonyinfos <tonylin49@gmail.com>
2018-04-16 00:07:14 +0800

    add sutitle file

--- play.js
+++ play.js
@@ -1,4 +1,4 @@
-//var my = new Object();
+  
 // loads the IFrame Player API code asynchronously.
   var tag = document.createElement('script');
 
@@ -21,46 +21,23 @@ $(document).ready(function () {
             console.log(my.captions);
     });
 });*/
-var captions = {"en":[{
-						"id":"27130157",
-						"caption_id":"127278",
-						"seq":"1",
-						"text":"The majority of americans believe in god.",
-						"translate_comment":"",
-						"start":"2.78",
-						"dur":"2.11",
-						"is_scores":null,
-						"created_at":"1521708113",
-						"updated_at":null
-					},{
-						"id":"27130158",
-						"caption_id":"127278",
-						"seq":"2",
-						"text":"But it's a different story in Britain.",
-						"translate_comment":"",
-						"start":"7.11",
-						"dur":"1.4",
-						"is_scores":null,
-						"created_at":"1521708113",
-						"updated_at":null
-					},{
-						"id":"27130159",
-						"caption_id":"127278",
-						"seq":"3",
-						"text":"Where in 2009, people of no religion outnumbered christians for the first time.",
-						"translate_comment":"",
-						"start":"9.429",
-						"dur":"4.92",
-						"is_scores":null,
-						"created_at":"1521708113",
-						"updated_at":null
-					}]};
-$(document).ready(function () {
-    var cols = ["id", "caption_id", "seq", "text", "translate_comment", "start", "dur", "is_scores", "created_at", "updated_at"];
-    for (var i = 0; i < captions.en.length; i++) {
-    	$('#subtitle').append('<a href="#" class="list-group-item">'+captions.en[i][cols[3]]+'</a>');
 
-  	}
+$(document).ready(function () {
+	var ob;
+	my_url = "./video_1.js";
+	$.getJSON(my_url, function(json) {
+		console.log(json.captions);
+		/*var cols = ["id", "caption_id", "seq", "text", "translate_comment", "start", "dur", "is_scores", "created_at", "updated_at"];
+    	for (var i = 0; i < json.en.length; i++) {
+    		$('#subtitle').append('<a href="#" class="list-group-item">'+ my.captions.en[i][cols[3]]+'</a>');
+  		}*/
+	});
+
+    //var cols = ["id", "caption_id", "seq", "text", "translate_comment", "start", "dur", "is_scores", "created_at", "updated_at"];
+    //for (var i = 0; i < my.captions.en.length; i++) {
+    //	$('#subtitle').append('<a href="#" class="list-group-item">'+ my.captions.en[i][cols[3]]+'</a>');
+
+  	//}
   	
 });
 
